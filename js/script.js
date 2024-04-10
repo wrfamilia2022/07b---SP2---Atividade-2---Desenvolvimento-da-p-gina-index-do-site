72450c58a868129008b7703dbdde3d85ba9c0410
wrfamilia2022 <110564078+wrfamilia2022@users.noreply.github.com>
2024-04-09 21:36:30 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ function topo (){
     function login(){
         var logado = 0;
         var usuario = document.getElementById("usuario").value; 
-        var usuario = document.getElementById("senha").value; 
+        var senha = document.getElementById("senha").value; 
 
         if(usuario == 'admin' && senha == '123456'){
             window.location = "index.html";
